3be84702c766b6ad65691adffec07c33144dda07
Tor020 <kopp020@gmail.com>
2018-03-17 17:22:43 -0400

    Initial Commit From Previous repo

---
+++ Dev/scripts/main.js
@@ -0,0 +1,43 @@
+
+
+
+      discordWidget.init({
+          serverId: '364473157312315394',
+          title: 'Devvit.io',
+          join: false,
+          alphabetical: true,
+          theme: 'light',
+          hideChannels: true,
+          showAllUsers: true,
+          allUsersDefaultState: false
+      });
+      discordWidget.render();
+
+
+(function (window, document) {
+
+const cb1 = document.getElementById('cb1');
+const cb2 = document.getElementById('cb2');
+
+const events = document.querySelectorAll('input[type=checkbox]');
+
+// events.addEventListener('change', bothChecked);
+events.forEach(cb => cb.addEventListener('change', bothChecked));
+
+
+function bothChecked(e){
+}
+
+
+
+
+  
+})(window, document);
+
+
+
+
+
+
+
+
